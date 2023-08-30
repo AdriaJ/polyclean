e072ab05e266e1b466b10d2a2ee2758da0d4863e
Adrian Jarret <adrian.jarret@epfl.ch>
2023-08-30 16:43:04 +0200

    Updates to save the produced images (not tested yet).

--- scripts/exp1/fill_one_row.sh
+++ scripts/exp1/fill_one_row.sh
@@ -1,15 +1,62 @@
 #!/bin/bash
 # Fill one row in the dataframe
 
-read -r rmax <<< "$1"
+# Simulate the problem and save ms file: Python
+#       -> save noisy ms file and ground truth source image + npix and seed
+#       -> Creates: data.ms, gtimage.pkl, rmax_npix_seed.pkl (dict), ws_args.txt (npix and cellsize on two lines)
+# Run the LASSO solvers: Python
+#       -> Open the ms file and perform the reconstruction, encapsulate everything in functions
+#       -> export output to pickle
+#       -> Creates: lasso_res.pkl (dictionary), lips_t.pkl (dict), clean_beam.pkl (dict), gt_conv.pkl (image)
+# WS-CLEAN: load, run, save and move results + time: bash
+#       -> save time to txt file
+#       -> Creates: ws-model.fits, ws-image.fits, ws-psf.fits, ws-residual.fits
+# post process ws-clean: Python
+#       -> load ws-clean data
+#       -> compute metrics and export to pickle
+#       -> Creates: clean_res.pkl (dict)
+# Fill the dataframe: Python  -----------------------------------------------
+#       -> load the pickle files and fill df
+# remove tmp folders
+
+# Initialize default values
+save="false"
+rmax=""
+
+# Process optional arguments
+while [ $# -gt 0 ]; do
+    case $1 in
+        --save)
+            save="true"
+            shift
+            ;;
+        *)
+            break  # Exit the loop when the first non-option argument is encountered
+            ;;
+    esac
+done
+
+# Assign remaining arguments
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [--save] <rmax>"
+    exit 1
+fi
+rmax="$1"
 
 echo "Simulation for rmax = $rmax"
 
 #conda activate pclean
 mkdir -p tmpdir
 python simulate_pb.py --rmax "$rmax"
-python solve_lasso.py  #todo -----------> to test now
-#load ws-clean parameters
+if [ $save = "true" ]; then
+    echo "Saving..."
+    save_dir="reconstructions/rmax_$rmax"
+    mkdir -p $save_dir
+   python solve_lasso.py --save_path "$save_dir"  #todo test --save_path
+else
+   python solve_lasso.py
+fi
+# load ws-clean parameters
 IFS=$'\n' read -r -d '' npix cellsize < tmpdir/ws_args.txt
 start_time="$(date -u +%s.%N)"
 wsclean -auto-threshold 1 -size "$npix" "$npix" -scale "$cellsize" -mgain 0.7 -niter 10000 -weight natural -name ws -quiet -no-dirty tmpdir/data.ms
@@ -18,7 +65,7 @@ elapsed="$(bc <<<"$end_time-$start_time")"
 echo "Total of $elapsed seconds elapsed for wsclean"
 echo $elapsed > tmpdir/wsclean_time.txt
 mv ws-* tmpdir
-python pp_wsclean.py  #todo -----------
+python pp_wsclean.py
 python fill_df.py
 rm -r -d tmpdir
 
@@ -26,23 +73,4 @@ rm -r -d tmpdir
 #mv --backup=t props.csv archive/props.csv
 #mv --backup=t metrics.csv archive/metrics.csv
 
-#todo update with Pyxu and the new lipschitz api + function scope for lasso reconstructions
-
-
-  # Simulate the problem and save ms file: Python
-  #       -> save noisy ms file and ground truth source image + npix and seed
-  #       -> Creates: data.ms, gtimage.pkl, rmax_npix_seed.pkl (dict), ws_args.txt (npix and cellsize on two lines)
-  # Run the LASSO solvers: Python
-  #       -> Open the ms file and perform the reconstruction, encapsulate everything in functions
-  #       -> export output to pickle
-  #       -> Creates: lasso_res.pkl (dictionary), lips_t.pkl (dict), clean_beam.pkl (dict), gt_conv.pkl (image)
-  # WS-CLEAN: load, run, save and move results + time: bash
-  #       -> save time to txt file
-  #       -> Creates: ws-model.fits, ws-image.fits, ws-psf.fits, ws-residual.fits
-  # post process ws-clean: Python
-  #       -> load ws-clean data
-  #       -> compute metrics and export to pickle
-  #       -> Creates: clean_res.pkl (dict)
-  # Fill the dataframe: Python  -----------------------------------------------
-  #       -> load the pickle files and fill df
-  # remove tmp folders
+#todo update with Pyxu and the new lipschitz api
--- scripts/exp1/solve_lasso.py
+++ scripts/exp1/solve_lasso.py
@@ -3,6 +3,7 @@ import os
 import yaml
 import time
 import numpy as np
+import argparse
 
 from astropy.coordinates import SkyCoord
 import astropy.units as u
@@ -29,6 +30,13 @@ if __name__ == "__main__":
         predicted_visi = pickle.load(file)  # Rascil Visibility instance
     with open(os.path.join(TMP_DATA_DIR, 'gtimage.pkl'), 'rb') as file:
         sky_im = pickle.load(file)  # Rascil Image instance
+
+    parser = argparse.ArgumentParser(
+        description='Performs the LASSO reconstructions with Python: PolyCLEAN, APGD and Monoatomic FW.')
+    parser.add_argument('--save_path', type=str, default=None, required=False,
+                        help="Location where to save the produced images. If None, the images are not saved.")
+    args = parser.parse_args()
+    save_images = args.save_path is not None
 
     uvwlambda = predicted_visi.visibility_acc.uvw_lambda.reshape(-1, 3)
     flags_bool = (predicted_visi.weight.data != 0.).reshape(-1)
@@ -70,105 +78,129 @@ if __name__ == "__main__":
     cb = fit_psf(psf)
     restored_sources = restore_cube(sky_im, None, None, clean_beam=cb)
 
-    # todo encapsulate every reconstruction in a function
-
     # PolyCLEAN
-    fit_params_pc = {
-        "stop_crit": pc.stop_crit(config['lasso_params']['tmax'],
-                                  config['lasso_params']['min_iter'],
-                                  config['lasso_params']['eps']),
-        "positivity_constraint": True,
-        "diff_lipschitz": fOp_lipschitz ** 2,
-        "precision_rule": lambda k: 10 ** (-k / 10),
-    }
-    pclean = pc.PolyCLEAN(
-        data=measurements,
-        uvwlambda=flagged_uvwlambda,
-        direction_cosines=direction_cosines,
-        lambda_=lambda_,
-        chunked=config['lasso_params']['chunked'],
-        nufft_eps=config['lasso_params']['nufft_eps'],
-        **config['pclean_params']
-    )
-    print("PolyCLEAN: Solving...")
-    pclean_time = time.time()
-    pclean.fit(**fit_params_pc)
-    print("\tSolved in {:.3f} seconds".format(dt_pclean := time.time() - pclean_time))
-    sol_pc, hist_pc = pclean.stats()
-    pclean_residual = forwardOp.adjoint(measurements - forwardOp(sol_pc["x"]))
-    pclean_comp = image_model.copy(deep=True)
-    pclean_comp.pixels.data[0, 0] = sol_pc["x"].reshape((npix,) * 2)
-    # pclean_residual_im = image_model.copy(deep=True)
-    # pclean_residual_im.pixels.data = pclean_residual.reshape(pclean_residual_im.pixels.data.shape) / sum_vis
+    def solve_pclean(save_im):
+        fit_params_pc = {
+            "stop_crit": pc.stop_crit(config['lasso_params']['tmax'],
+                                      config['lasso_params']['min_iter'],
+                                      config['lasso_params']['eps']),
+            "positivity_constraint": True,
+            "diff_lipschitz": fOp_lipschitz ** 2,
+            "precision_rule": lambda k: 10 ** (-k / 10),
+        }
+        pclean = pc.PolyCLEAN(
+            data=measurements,
+            uvwlambda=flagged_uvwlambda,
+            direction_cosines=direction_cosines,
+            lambda_=lambda_,
+            chunked=config['lasso_params']['chunked'],
+            nufft_eps=config['lasso_params']['nufft_eps'],
+            **config['pclean_params']
+        )
+        print("PolyCLEAN: Solving...")
+        pclean_time = time.time()
+        pclean.fit(**fit_params_pc)
+        print("\tSolved in {:.3f} seconds".format(dt_pclean := time.time() - pclean_time))
+        sol_pc, hist_pc = pclean.stats()
+        pclean_residual = forwardOp.adjoint(measurements - forwardOp(sol_pc["x"]))
+        pclean_comp = image_model.copy(deep=True)
+        pclean_comp.pixels.data[0, 0] = sol_pc["x"].reshape((npix,) * 2)
+        restored_comp = restore_cube(pclean_comp, None, None, clean_beam=cb)
+        if save_im:
+            restored_comp.image_acc.export_to_fits(os.path.join(args.save_path, 'pclean_comp_restored.fits'))
+            pclean_residual_im = image_model.copy(deep=True)
+            pclean_residual_im.pixels.data = pclean_residual.reshape(pclean_residual_im.pixels.data.shape) / sum_vis
+            restored = restore_cube(pclean_comp, None, pclean_residual_im, clean_beam=cb)
+            restored.image_acc.export_to_fits(os.path.join(args.save_path, 'pclean_restored.fits'))
+        # compute mse and mad between restored comp and gt convolved
+        mse_pc = ut.MSE(restored_comp, restored_sources)
+        mad_pc = ut.MAD(restored_comp, restored_sources)
 
-    restored_comp = restore_cube(pclean_comp, None, None, clean_beam=cb)
-    # compute mse and mad between restored comp and gt convolved
-    mse_pc = ut.MSE(restored_comp, restored_sources)
-    mad_pc = ut.MAD(restored_comp, restored_sources)
+        res_pc = dict(zip(metrics, [dt_pclean, mse_pc, mad_pc, hist_pc["Memorize[objective_func]"][-1], sol_pc['dcv']]))
 
-    res_pc = dict(zip(metrics, [dt_pclean, mse_pc, mad_pc, hist_pc["Memorize[objective_func]"][-1], sol_pc['dcv']]))
+        return res_pc, hist_pc, dt_pclean
 
+    res_pc, hist_pc, dt_pclean = solve_pclean(save_images)
 
     # APGD
-    fit_params_apgd = {
-        "x0": np.zeros(forwardOp.shape[1], dtype="float64"),
-        "stop_crit": pc.stop_crit(40 * dt_pclean, config['lasso_params']['min_iter'], config['lasso_params']['eps'],
-                                    value=hist_pc["Memorize[objective_func]"][-1]),
-        # APGD stops when the objective function is a good as PolyCLEAN's one
-        "track_objective": True,
-        "tau": 1 / (fOp_lipschitz ** 2)
-    }
-    data_fid_synth = 0.5 * pycop.SquaredL2Norm(dim=forwardOp.shape[0]).argshift(-measurements) * forwardOp
-    regul_synth = lambda_ * pyfwl.L1NormPositivityConstraint(shape=(1, None))
-    apgd = pycsol.PGD(data_fid_synth, regul_synth, show_progress=False)
-    print("APGD: Solving ...")
-    start = time.time()
-    apgd.fit(**fit_params_apgd)
-    print("\tSolved in {:.3f} seconds".format(dt_apgd := time.time() - start))
-    sol_apgd, _ = apgd.stats()
-    apgd_residual = forwardOp.adjoint(measurements - forwardOp(sol_apgd["x"]))
-    dcv_apgd = abs(apgd_residual).max() / lambda_
-    apgd_comp = image_model.copy(deep=True)
-    apgd_comp.pixels.data[0, 0] = sol_apgd["x"].reshape((npix,) * 2)
-    # apgd_residual_im = image_model.copy(deep=True)
-    # apgd_residual_im.pixels.data = apgd_residual.reshape(apgd_residual_im.pixels.data.shape) / sum_vis
+    def solve_apgd(save_im):
+        fit_params_apgd = {
+            "x0": np.zeros(forwardOp.shape[1], dtype="float64"),
+            "stop_crit": pc.stop_crit(40 * dt_pclean, config['lasso_params']['min_iter'], config['lasso_params']['eps'],
+                                        value=hist_pc["Memorize[objective_func]"][-1]),
+            # APGD stops when the objective function is a good as PolyCLEAN's one
+            "track_objective": True,
+            "tau": 1 / (fOp_lipschitz ** 2)
+        }
+        data_fid_synth = 0.5 * pycop.SquaredL2Norm(dim=forwardOp.shape[0]).argshift(-measurements) * forwardOp
+        regul_synth = lambda_ * pyfwl.L1NormPositivityConstraint(shape=(1, None))
+        apgd = pycsol.PGD(data_fid_synth, regul_synth, show_progress=False)
+        print("APGD: Solving ...")
+        start = time.time()
+        apgd.fit(**fit_params_apgd)
+        print("\tSolved in {:.3f} seconds".format(dt_apgd := time.time() - start))
+        sol_apgd, _ = apgd.stats()
+        apgd_residual = forwardOp.adjoint(measurements - forwardOp(sol_apgd["x"]))
+        dcv_apgd = abs(apgd_residual).max() / lambda_
+        apgd_comp = image_model.copy(deep=True)
+        apgd_comp.pixels.data[0, 0] = sol_apgd["x"].reshape((npix,) * 2)
+        restored_comp = restore_cube(apgd_comp, None, None, clean_beam=cb)
+        if save_im:
+            restored_comp.image_acc.export_to_fits(os.path.join(args.save_path, 'apgd_comp_restored.fits'))
+            apgd_residual_im = image_model.copy(deep=True)
+            apgd_residual_im.pixels.data = apgd_residual.reshape(apgd_residual_im.pixels.data.shape) / sum_vis
+            restored = restore_cube(apgd_comp, None, apgd_residual_im, clean_beam=cb)
+            restored.image_acc.export_to_fits(os.path.join(args.save_path, 'apgd_restored.fits'))
 
-    restored_comp = restore_cube(apgd_comp, None, None, clean_beam=cb)
-    # compute mse and mad between restored comp and gt convolved
-    mse_apgd = ut.MSE(restored_comp, restored_sources)
-    mad_apgd = ut.MAD(restored_comp, restored_sources)
+        # compute mse and mad between restored comp and gt convolved
+        mse_apgd = ut.MSE(restored_comp, restored_sources)
+        mad_apgd = ut.MAD(restored_comp, restored_sources)
 
-    res_apgd = dict(zip(metrics, [dt_apgd, mse_apgd, mad_apgd, apgd.objective_func(), dcv_apgd]))
+        res_apgd = dict(zip(metrics, [dt_apgd, mse_apgd, mad_apgd, apgd.objective_func()[0], dcv_apgd]))
+
+        return res_apgd
+
+    res_apgd = solve_apgd(save_images)
 
     # Monoatomic FW
-    monofw = pc.MonoFW(
-        data=measurements,
-        uvwlambda=flagged_uvwlambda,
-        direction_cosines=direction_cosines,
-        lambda_=lambda_,
-        chunked=config['lasso_params']['chunked'],
-        nufft_eps=config['lasso_params']['nufft_eps'],
-        **config['monofw_params']
-    )
-    print("Monoatomic FW: Solving ...")
-    start = time.time()
-    monofw.fit(stop_crit=pc.stop_crit(config['monofw_params']['max_time_factor'] * dt_pclean, config['lasso_params']['min_iter'], config['lasso_params']['eps'],
-                                        value=hist_pc["Memorize[objective_func]"][-1]))
-    print("\tSolved in {:.3f} seconds".format(dt_monofw := time.time() - start))
-    sol_monofw, _ = monofw.stats()
-    monofw_residual = forwardOp.adjoint(measurements - forwardOp(sol_monofw["x"]))
-    dcv_monofw = abs(monofw_residual).max() / lambda_
-    monofw_comp = image_model.copy(deep=True)
-    monofw_comp.pixels.data[0, 0] = sol_monofw["x"].reshape((npix,) * 2)
-    # monofw_residual_im = image_model.copy(deep=True)
-    # monofw_residual_im.pixels.data = monofw_residual.reshape(monofw_residual_im.pixels.data.shape) / sum_vis
+    def res_monofw(save_im):
+        monofw = pc.MonoFW(
+            data=measurements,
+            uvwlambda=flagged_uvwlambda,
+            direction_cosines=direction_cosines,
+            lambda_=lambda_,
+            chunked=config['lasso_params']['chunked'],
+            nufft_eps=config['lasso_params']['nufft_eps'],
+            **config['monofw_params']
+        )
+        print("Monoatomic FW: Solving ...")
+        start = time.time()
+        monofw.fit(stop_crit=pc.stop_crit(config['monofw_params']['max_time_factor'] * dt_pclean, config['lasso_params']['min_iter'], config['lasso_params']['eps'],
+                                            value=hist_pc["Memorize[objective_func]"][-1]))
+        print("\tSolved in {:.3f} seconds".format(dt_monofw := time.time() - start))
+        sol_monofw, _ = monofw.stats()
+        monofw_residual = forwardOp.adjoint(measurements - forwardOp(sol_monofw["x"]))
+        dcv_monofw = abs(monofw_residual).max() / lambda_
+        monofw_comp = image_model.copy(deep=True)
+        monofw_comp.pixels.data[0, 0] = sol_monofw["x"].reshape((npix,) * 2)
 
-    restored_comp = restore_cube(monofw_comp, None, None, clean_beam=cb)
-    # compute mse and mad between restored comp and gt convolved
-    mse_monofw = ut.MSE(restored_comp, restored_sources)
-    mad_monofw = ut.MAD(restored_comp, restored_sources)
 
-    res_monofw = dict(zip(metrics, [dt_monofw, mse_monofw, mad_monofw, monofw.objective_func(), dcv_monofw]))
+        restored_comp = restore_cube(monofw_comp, None, None, clean_beam=cb)
+        if save_im:
+            restored_comp.image_acc.export_to_fits(os.path.join(args.save_path, 'monofw_comp_restored.fits'))
+            monofw_residual_im = image_model.copy(deep=True)
+            monofw_residual_im.pixels.data = monofw_residual.reshape(monofw_residual_im.pixels.data.shape) / sum_vis
+            restored = restore_cube(monofw_comp, None, monofw_residual_im, clean_beam=cb)
+            restored.image_acc.export_to_fits(os.path.join(args.save_path, 'monofw_restored.fits'))
+        # compute mse and mad between restored comp and gt convolved
+        mse_monofw = ut.MSE(restored_comp, restored_sources)
+        mad_monofw = ut.MAD(restored_comp, restored_sources)
+
+        res_monofw = dict(zip(metrics, [dt_monofw, mse_monofw, mad_monofw, monofw.objective_func()[0], dcv_monofw]))
+
+        return res_monofw
+
+    res_monofw = res_monofw(save_images)
 
     lasso_res = dict(zip(['pclean', 'apgd', 'monofw'], [res_pc, res_apgd, res_monofw]))
 
